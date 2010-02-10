92214418f0db6fa382ffdf004072354e2e32279c
Alex Harvey <alex@pixelfactor.ca>
2010-02-09 19:57:52 -0800

    Added all the rewrite to the tv using pySerial

--- LGtv.py
+++ LGtv.py
@@ -1,3 +1,5 @@
+import serial
+
 class LGtv():
     
     INPUT_DTV_ANT = 0x00
@@ -48,129 +50,138 @@ class LGtv():
     ENERGY_SAVING_MAXIMUM = 0x3
     ENERGY_SAVING_SCREEN_OFF = 0x5
 
-    def __init__(self, id=1):
+    def __init__(self, serialport, id=1):
         self.id = "%02X" % id
-    
+        self.ser = serial.Serial(serialport, 9600, timeout=1)
+
+    def __sendCmd(self, cmd):
+        self.ser.write(cmd)
+        read = self.ser.readline()
+        if read.find("OK") > 0:
+            return True
+        else:
+            return False
+
     def powerOff(self):
         """Turn the TV off"""
-        return 'ka %s %02X\r' % (self.id, 0x0)
+        return self.__sendCmd('ka %s %02X\r' % (self.id, 0x0))
     
     def powerOn(self):
         """Turn the TV on"""
-        return 'ka %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('ka %s %02X\r' % (self.id, 0x1))
     
     def inputSelect(self, input):
         """Select the TV's input"""
-        return 'xb %s %02X\r' % (self.id, input)
+        return self.__sendCmd('xb %s %02X\r' % (self.id, input))
     
     def ratio(self, ratio):
         """Select the TV's ratio"""
-        return 'xc %s %02X\r' % (self.id, ratio)
+        return self.__sendCmd('xc %s %02X\r' % (self.id, ratio))
     
     def screenMuteOff(self):
         """Screen mute off (video on)"""
-        return 'kd %s %02X\r' % (self.id, 0x0)
+        return self.__sendCmd('kd %s %02X\r' % (self.id, 0x0))
     
     def screenMuteOn(self):
         """Screen mute on (video off)"""
-        return 'kd %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('kd %s %02X\r' % (self.id, 0x1))
     
     def screenMuteVideoOut(self):
         """Screen mute video out, but video is on"""
-        return 'kd %s %02X\r' % (self.id, 0x10)
+        return self.__sendCmd('kd %s %02X\r' % (self.id, 0x10))
     
     def volumeMuteOn(self):
         """Mute TV volume"""
-        return 'ke %s %02X\r' % (self.id, 0x0)
+        return self.__sendCmd('ke %s %02X\r' % (self.id, 0x0))
     
     def volumeMuteOff(self):
         """Unmute TV volume"""
-        return 'ke %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('ke %s %02X\r' % (self.id, 0x1))
     
     def setVolume(self, level):
         """Set the volume, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kf %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kf %s %02X\r' % (self.id, level))
     
     def setContrast(self, level):
         """Set the contrast, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kg %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kg %s %02X\r' % (self.id, level))
 
     def setBrightness(self, level):
         """Set the brightness, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kh %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kh %s %02X\r' % (self.id, level))
     
     def setColor(self, level):
         """Set the color, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'ki %s %02X\r' % (self.id, level)
+        return self.__sendCmd('ki %s %02X\r' % (self.id, level))
 
     def setTint(self, level):
         """Set the tint, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kj %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kj %s %02X\r' % (self.id, level))
 
     def setSharpness(self, level):
         """Set the sharpness, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kk %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kk %s %02X\r' % (self.id, level))
     
     def OSDoff(self):
         """Turn off OSD"""
-        return 'kl %s %02X\r' % (self.id, 0x0)
+        return self.__sendCmd('kl %s %02X\r' % (self.id, 0x0))
     
     def OSDon(self):
         """Turn on OSD"""
-        return 'kl %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('kl %s %02X\r' % (self.id, 0x1))
 
     def remoteControlLockModeOff(self):
         """Turn remote control lock off"""
-        return 'km %s %02X\r' % (self.id, 0x0)
+        return self.__sendCmd('km %s %02X\r' % (self.id, 0x0))
     
     def remoteControlLockModeOn(self):
         """Turn remote control lock on"""
-        return 'km %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('km %s %02X\r' % (self.id, 0x1))
     
     def setTreble(self, level):
         """Set the treble, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kr %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kr %s %02X\r' % (self.id, level))
 
     def setBass(self, level):
         """Set the bass, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'ks %s %02X\r' % (self.id, level)
+        return self.__sendCmd('ks %s %02X\r' % (self.id, level))
     
     def setBalance(self, level):
         """Set the balance, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'kt %s %02X\r' % (self.id, level)
+        return self.__sendCmd('kt %s %02X\r' % (self.id, level))
     
     def setColorTemperature(self, color):
         """Set the color temperature"""
-        return 'ku %s %02X\r' % (self.id, color)
+        return self.__sendCmd('ku %s %02X\r' % (self.id, color))
     
     def setEnergySaving(self, energy):
         """Set the energy saving"""
-        return 'jq %s %02X\r' % (self.id, energy)
+        return self.__sendCmd('jq %s %02X\r' % (self.id, energy))
     
     def autoConfig(self):
         """Trigger auto configuration"""
-        return 'ju %s %02X\r' % (self.id, 0x1)
+        return self.__sendCmd('ju %s %02X\r' % (self.id, 0x1))
 
     def setBacklight(self, level):
         """Set the backlight, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
-        return 'mg %s %02X\r' % (self.id, level)
+        return self.__sendCmd('mg %s %02X\r' % (self.id, level))
